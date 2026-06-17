2e47a979ef488df8356c755627f6842d2dda25d5
Brian Guthrie <brian@forgetmenotcc.com>
2026-06-17 16:57:36 -0600

    Refactor: player to hash

--- app/main.rb
+++ app/main.rb
@@ -1,61 +1,56 @@
 def tick args
-  args.state.player_x ||= 120
-  args.state.player_y ||= 280
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: 100,
+    h: 80,
+    speed: 12,
+    path: 'sprites/misc/dragon-0.png',
+  }
   args.state.fireballs ||= []
 
-  speed = 12
-  player_w = 100
-  player_h = 80
-
   if args.inputs.left
-    args.state.player_x -= speed
+    args.state.player.x -= args.state.player.speed
   elsif args.inputs.right
-    args.state.player_x += speed
+    args.state.player.x += args.state.player.speed
   end
 
   if args.inputs.up
-    args.state.player_y += speed
+    args.state.player.y += args.state.player.speed
   elsif args.inputs.down
-    args.state.player_y -= speed
+    args.state.player.y -= args.state.player.speed
   end
 
-  if args.state.player_x +  player_w > args.grid.w
-    args.state.player_x = args.grid.w - player_w
+  if args.state.player.x +  args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
   end
 
-  if args.state.player_x < 0
-    args.state.player_x = 0
+  if args.state.player.x < 0
+    args.state.player.x = 0
   end
 
-  if args.state.player_y + player_h > args.grid.h
-    args.state.player_y = args.grid.h - player_h
+  if args.state.player.y + args.state.player.h > args.grid.h
+    args.state.player.y = args.grid.h - args.state.player.h
   end
 
-  if args.state.player_y < 0
-    args.state.player_y = 0
+  if args.state.player.y < 0
+    args.state.player.y = 0
   end
 
   if args.inputs.keyboard.key_down.z ||
       args.inputs.keyboard.key_down.j ||
       args.inputs.controller_one.key_down.a
     args.state.fireballs << {
-      x: args.state.player_x,
-      y: args.state.player_y,
+      x: args.state.player.x,
+      y: args.state.player.y,
       text: 'fireball',
     }
   end
 
   args.state.fireballs.each do |fireball|
-    fireball.x += speed + 2
+    fireball.x += args.state.player.speed + 2
   end
 
   args.outputs.labels << args.state.fireballs
-
-  args.outputs.sprites << {
-    x: args.state.player_x,
-    y: args.state.player_y,
-    w: player_w,
-    h: player_h,
-    path: 'sprites/misc/dragon-0.png',
-  }
+  args.outputs.sprites << args.state.player
 end
